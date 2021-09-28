d2dd5c655317354c8def3fa77a7710f1d422615a
DongSub_Joung <joungdongsub1103@gmail.com>
2021-09-28 17:58:58 +0900

    Update UserInfo.cpp
    REFECTOR: HAVE THE PURE STRUCT

--- server_project/server_projectVer0.4/UserInfo.cpp
+++ server_project/server_projectVer0.4/UserInfo.cpp
@@ -1,5 +1,5 @@
 /*!
-*@file	UserInfo.h
+*@file	UserInfo.cp
 *@todo
 *		new PreUser;
 *		-> new User= PreUser;
@@ -14,21 +14,17 @@ class UserInfo
 {
 public:
 	UserInfo(){}
-	User getUser();
-	void moveUserData()User* preuser_info, UserInfo* newUser);
 private:
 	struct User;
 	struct UserInfo : User;
-	void setPreuser(string id, string PW, string my_number);
-	void moveUserData()User* preuser_info, UserInfo* newUser);
 }
 
 
-struct User
+struct UserInfo::User
 {
-	string& m_user_ID{""}
-	string& m_user_PW{""}
-	string& my_number{""}
+	string* m_user_ID{""}
+	string* m_user_PW{""}
+	string* my_number{""}
 	map<string, bool> mark_users;
 
 	User(string id, string PW, string my_number)
@@ -37,12 +33,12 @@ struct User
 		m_user_PW {PW};
 		my_number {my_number};
 
-		mark_users= new map<string, map>;
+		mark_users= new map<string, bool>;
 		mark_users.set(id, false);
 	}
 };
 
-struct UserInfo : User
+struct UserInfo::approvedUsers : User
 {
 	enum ACCESS{
 		READ= 0,
@@ -54,7 +50,7 @@ struct UserInfo : User
 	int m_authority_level[3]{ 0,0,0 };
 
 	// Sample levle value "111" or "101"
-	UserInfo(string level)
+	approvedUsers(string level)
 	{
 		try
 		{
@@ -72,22 +68,3 @@ struct UserInfo : User
 		}
 	}
 };
-
-void UserInfo::setPreuser(string id, string PW, string my_number)
-{
-	User pre_user= new User(id, PW, my_number); 
-}
-
-UserInfo UserInfo::getUser()
-{
-	return Userinfo;
-}
-
-// @TODO
-// Deep copy struct value/
-void moveUserData(User* preuser_info, UserInfo* newUser)
-{
-	preuser_info->m_user_ID = newUser->m_user_ID;
-	preuser_info->m_user_PW = newUser->m_user_PW;
-	preuser_info->m_user_ID_number = newUser->m_user_ID_number;
-}
