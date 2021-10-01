146bcfb49e7753fadb7ae15fc74320e8ab0e32be
dongsub-joung <joungdongsub1103@gmail.com>
2021-10-01 23:05:03 +0900

    COMPLETE user handler

--- server_project/server_projectVer0.4/UserInfoHandler.cpp
+++ server_project/server_projectVer0.4/UserInfoHandler.cpp
@@ -14,19 +14,14 @@ public:
     vector<string> getPreuserMynumber(vector<UserInfo::User> users);
 
 private:
-    UserInfo::User*           unapproved_users;
-    UserInfo::approvedUsers*  approved_users;
-    void moveUserData(User* preuser_info, UserInfo* newUser);
+    UserInfo::User           unapproved_users;
+    UserInfo::approvedUsers  approved_users;
+    void moveUserData(User unapproved_users, UserInfo approved_users, string lvl);
 }
 
 void UserInfoHandler::setPreuser(string id, string PW, string my_number)
 {
     unapproved_users= new User(id, PW, my_number);
-}
-
-void UserInfoHandler::setApproveUser(string level)
-{
-    approved_users= new approvedUsers(level);
 }
 
 vector<string> UserInfoHandler::getPreuserID(vector<UserInfo::User> users)
@@ -58,11 +53,18 @@ approvedUsers UserInfoHandler::getApprovedUser()
 	return approved_users;
 }
 
-void UserInfoHandler::moveUserData(stirng id)
+void UserInfoHandler::moveUserDatavoid moveUserData(User unapproved_users, string id, string lvl)
 {
-    // @TODO
-    // unapproved_users::mark_users.MODIFY(id, true);
-    
+    bool id_checker{false};
+
+    id_checker= unapproved_users.count(id) : true ? false;
+    if(id_checker)
+    {	
+    	unapproved_users.mark_users[id]= true;
+    	approved_users= UserInfo::approveUsers approvedUsers(lvl);
+    }
+    else { cout<< "UnJoined ID\n" << endl; return; }
+  
     // approved_users= unapproved_users;
     approved_users.m_user_ID= unapproved_users.m_user_ID;
     approved_users.m_user_PW= unapproved_users.m_user_PW;
